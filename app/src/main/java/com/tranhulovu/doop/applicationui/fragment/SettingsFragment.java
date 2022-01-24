708279b05152fca183d5511548c6a7a6ad86cbf2
npdanhs <npdanhs@gmail.com>
2022-01-24 15:56:39 +0700

    add settings and statistic UI

--- app/src/main/java/com/tranhulovu/doop/applicationui/fragment/SettingsFragment.java
+++ app/src/main/java/com/tranhulovu/doop/applicationui/fragment/SettingsFragment.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,6 +14,7 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
 import com.tranhulovu.doop.MainActivity;
+import com.google.android.material.card.MaterialCardView;
 import com.tranhulovu.doop.R;
 
 public class SettingsFragment extends ManagerFragment {
@@ -25,13 +29,72 @@ public class SettingsFragment extends ManagerFragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        navController = Navigation.findNavController(view);
-        view.findViewById(R.id.settings_logOut).setOnClickListener(new View.OnClickListener() {
+
+        MaterialCardView notification = view.findViewById(R.id.settings_notification);
+        ImageView notificationIcon = view.findViewById(R.id.settings_notification_icon);
+        TextView notificationState = view.findViewById(R.id.settings_notification_state);
+
+        notification.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
+                if (notificationState.getText() == "ON"){
+                    notificationIcon.setImageResource(R.drawable.ic_notification_off);
+                    notificationState.setText("OFF");
+                }
+                else {
+                    notificationIcon.setImageResource(R.drawable.ic_notification_on);
+                    notificationState.setText("ON");
+                }
+            }
+        });
+
+        MaterialCardView autoArchiveCard = view.findViewById(R.id.settings_autoArchiveCard);
+        ImageView autoArchiveCardIcon = view.findViewById(R.id.settings_autoArchiveCard_icon);
+        TextView autoArchiveCardState = view.findViewById(R.id.settings_autoArchiveCard_state);
+
+        autoArchiveCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (autoArchiveCardState.getText() == "ON"){
+                    autoArchiveCardIcon.setImageResource(R.drawable.ic_auto_archive_card_off);
+                    autoArchiveCardState.setText("OFF");
+                }
+                else {
+                    autoArchiveCardIcon.setImageResource(R.drawable.ic_auto_archive_card_on);
+                    autoArchiveCardState.setText("ON");
+                }
+            }
+        });
+
+        MaterialCardView timeSetting = view.findViewById(R.id.settings_timeSetting);
+        timeSetting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RelativeLayout timesetting = view.findViewById(R.id.settings_timeSetting_timeformat_layout);
+                RelativeLayout datesetting = view.findViewById(R.id.settings_timeSetting_dateformat_layout);
+                timesetting.setVisibility((timesetting.getVisibility() == View.VISIBLE) ? View.GONE : View.VISIBLE);
+                datesetting.setVisibility((datesetting.getVisibility() == View.VISIBLE) ? View.GONE : View.VISIBLE);
+            }
+        });
+
+        MaterialCardView about = view.findViewById(R.id.settings_about);
+        about.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView about_info = view.findViewById(R.id.settings_about_info);
+                about_info.setVisibility((about_info.getVisibility() == View.VISIBLE) ? View.GONE : View.VISIBLE);
+            }
+        });
+
+        navController = Navigation.findNavController(view);
+        MaterialCardView logoutButton = view.findViewById(R.id.settings_logOut);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 MainActivity.getAuthenticator().requestSignOut();
                 navController.navigate(R.id.action_mainFragment_to_authenticatorFragment);
             }
         });
+
     }
 }
--- app/src/main/java/com/tranhulovu/doop/applicationui/fragment/StatisticsFragment.java
+++ app/src/main/java/com/tranhulovu/doop/applicationui/fragment/StatisticsFragment.java
@@ -1,5 +1,6 @@
 package com.tranhulovu.doop.applicationui.fragment;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,13 +9,51 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.tranhulovu.doop.R;
+
+import java.util.ArrayList;
 
 public class StatisticsFragment extends ManagerFragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.statisticsfragment, container, false);
+
+        PieChart pieChart_total = view.findViewById(R.id.staticstic_total_chart);
+        PieChart pieChart_thismonth = view.findViewById(R.id.staticstic_thismonth_chart);
+        PieChart pieChart_lastmonth = view.findViewById(R.id.staticstic_lastmonth_chart);
+
+        ArrayList<PieEntry> numcards = new ArrayList<>();
+        numcards.add(new PieEntry(10, "Missed"));
+        numcards.add(new PieEntry(15, "Done"));
+        numcards.add(new PieEntry(25, "Ongoing"));
+
+        PieDataSet pieDataSet = new PieDataSet(numcards, "Cards");
+        pieDataSet.setColors(ColorTemplate.LIBERTY_COLORS);
+        pieDataSet.setValueTextColor(Color.BLACK);
+        pieDataSet.setValueTextSize(16f);
+
+        PieData pieData = new PieData(pieDataSet);
+        pieChart_total.setData(pieData);
+        pieChart_total.getDescription().setEnabled(false);
+        pieChart_total.setCenterText("50\nCards");
+        pieChart_total.animate();
+
+        pieChart_thismonth.setData(pieData);
+        pieChart_thismonth.getDescription().setEnabled(false);
+        pieChart_thismonth.setCenterText("50\nCards");
+        pieChart_thismonth.animate();
+
+        pieChart_thismonth.setData(pieData);
+        pieChart_thismonth.getDescription().setEnabled(false);
+        pieChart_thismonth.setCenterText("50\nCards");
+        pieChart_thismonth.animate();
+
         return view;
     }
 }
